57a48ba86d59d88ddd2f69cba72526c10bdc4add
Hilquias Abias <76264931+HilquiasAbias@users.noreply.github.com>
2024-11-19 13:48:14 -0300

    Update script.py

--- script.py
+++ script.py
@@ -56,10 +56,10 @@ class DigitalPhotoFrame:
 if __name__ == "__main__":
     # Substitua pelos URLs das imagens que deseja exibir
     image_urls = [
-        "https://images.pexels.com/photos/4790267/pexels-photo-4790267.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/15470542/pexels-photo-15470542/free-photo-of-tecnologia-equipamento-maquinario-aparelhos.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/10635975/pexels-photo-10635975.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        "https://images.pexels.com/photos/22610381/pexels-photo-22610381/free-photo-of-smartphone-internet-conexao-ligacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        "https://i.pinimg.com/736x/1e/58/cf/1e58cf797711d792d9a09acb50d119ee.jpg",
+        "https://i.pinimg.com/736x/a9/3b/9a/a93b9ab1608030c238cfbbc8305f86b3.jpg",
+        "https://i.pinimg.com/736x/cc/fa/fa/ccfafa9b0bbaa711ff910cd362f67c1b.jpg",
+        "https://i.pinimg.com/736x/fe/b5/74/feb574e78a04d30587fc651702a6d687.jpg"
     ]
     try:
         app = DigitalPhotoFrame(image_urls)
